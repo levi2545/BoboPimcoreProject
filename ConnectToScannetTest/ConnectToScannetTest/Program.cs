527917c8e51200f1544306d2459f6394630b83f2
Philip Braarup Savage <psavage1995@gmail.com>
2020-08-25 08:43:20 +0200

    Created class that connects to Scannet and gets product info
    Title

---
+++ ConnectToScannetTest/ConnectToScannetTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using ScannetOnline;
+
+namespace ConnectToScannetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = test();
+            Console.WriteLine(c.Length);
+            foreach (ScannetOnline.Product item in c)
+            {
+                Console.WriteLine("ItemNumber: " + item.Id);
+            }
+            Console.ReadLine();
+        }
+
+        private static Product[] test()
+        {
+            ScannetOnline.WebServicePortClient client = new WebServicePortClient();
+
+            client.Solution_Connect("api@boboonline.dk", "Bobo8902");
+            client.Solution_SetLanguage("DK");
+            var result = client.Product_GetAll();
+            return result;
+        }
+    }
+}
